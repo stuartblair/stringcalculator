fe9566fe53edfce2067966b2c8d4954599df35b2
Stuart Blair <stuart.a.blair@gmail.com>
2013-02-15 06:18:21 +0000

    The calc method was getting a little on the long side and also had too much nesting, so I refactored it to introduce a couple of new explaining methods. This also means that the calc method has fallen in line with the composed method philosophy in which the methods are small and each statement within the method is at the same level of abstraction.

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,15 +3,19 @@ class StringCalculator
 		if input.empty? then 
 			return 0 
 		else
-			if contains_negative_number(input) then
-				raise RuntimeError, "Negative number found in the input"
-			else
-				eval(replace_commas_and_newlines_with_pluses(input))
-			end
+			return evaluated_string(input)
 		end
 	end
 
 	private
+
+	def evaluated_string(input)
+		if contains_negative_number(input) then
+			raise RuntimeError, "Negative number found in the input"
+		else
+			eval(replace_commas_and_newlines_with_pluses(input))
+		end
+	end		
 
 	def replace_commas_and_newlines_with_pluses(input)
 		input.gsub(/[,|\n]/, '+')	
