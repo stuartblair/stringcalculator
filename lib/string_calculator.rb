69b3260bffb5e0b33abca3e4b91b1629a2a01047
Stuart Blair <stuart.a.blair@gmail.com>
2013-02-15 03:44:24 +0000

    With that refactoring, the implementation is a little easier to read, but it's not quite as obvious as I'd like it. Let's see what influence the remaining tests have.

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,15 @@
 class StringCalculator
 	def calc(input)
-		if input.empty? then return 0 end
-		eval(input.gsub(/[,|\n]/, '+'))
+		if input.empty? then 
+			return 0 
+		else
+			eval(replace_commas_and_newlines_with_pluses(input))
+		end
+	end
+
+	private
+
+	def replace_commas_and_newlines_with_pluses(input)
+		input.gsub(/[,|\n]/, '+')	
 	end
 end
