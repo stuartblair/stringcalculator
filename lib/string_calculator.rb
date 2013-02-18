19b70d3ee1f4bfed4e024b3f9da6fa4bd3d57183
Stuart Blair <stuart.a.blair@gmail.com>
2013-02-18 19:30:26 +0000

    Looking at the StringCalculator, a lot of the methods aren't to do with calculation in strings as such. Instead, they're to do with reading the cutom delimiters from the string and then returning a list of tokens for the rest of the StringCalculator to then add together, throw an exception over or ignore completely.
    It looks like this class is doing too much, so I've introduced a
    Tokenizer. As I did so, I realized that it was a bit of a specific
    Tokenizer in that it uses an uncommon convention for specifying
    delimiters as part of the token input itself. Since I don't want to risk
    someone picking this up, I've introduced a StringCalculator module to
    wrap up both the StringCalculator and Tokenizer classes.
    
    I've started by moving the functionality and tests associated with
    returning tokens from the input into their own class and spec. I then
    refactored the StringCalculator to use my new Tokenizer.
    
    At this stage StringCalculator is really a test that integrates the
    functionality of both the StringCalculator and the Tokenizer.
    
    All the tests passed, so we're good to continue.

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,48 +1,34 @@
-class StringCalculator
-	def calc(input)
-		valid_numbers_from(elements(input)).inject(0) do |sum, number| 
-			sum + number 
-		end 
-	end
-
-	private
-
-	def valid_numbers_from(elements)
-		elements.map { |element| valid_number_from(numeric_value_of(element)) }
-	end
-
-	def elements(input)
-		input.split(delimiter_pattern(input))
-	end
-
-	def delimiter_pattern(input)
-		Regexp.new("[,|\n#{custom_pattern(input)}]")
-	end
-
-	def custom_pattern(input)
-		if (custom_delimiter(input)) then
-			"|#{custom_delimiter(input)}"
-		else 
-			""
+module StringCalculator
+	class StringCalculator
+		def calc(input)
+			valid_numbers_from(elements(input)).inject(0) do |sum, number| 
+				sum + number 
+			end 
 		end
-	end
 
-	def custom_delimiter(input)
-		find_custom_delimiter_pattern = /\/\/(.).*/ 
-		input.match(find_custom_delimiter_pattern)[1] unless !input.match(find_custom_delimiter_pattern)
-	end
+		private
 
-	def valid_number_from(number)
-		if number < 0 then
-			raise RuntimeError, "Negative number found in the input"
-		elsif number <= 1000 then 
-			number
-		else 
-			0 
+		def valid_numbers_from(elements)
+			elements.map { |element| valid_number_from(numeric_value_of(element)) }
 		end
-	end
 
-	def numeric_value_of(number_text)
-		number_text.to_i
+		def elements(input)
+			Tokenizer.new.tokens(input)
+		end
+
+		def valid_number_from(number)
+			if number < 0 then
+				raise RuntimeError, "Negative number found in the input"
+			elsif number <= 1000 then 
+				number
+			else 
+				0 
+			end
+		end
+
+		def numeric_value_of(number_text)
+			number_text.to_i
+		end
 	end
 end
+
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,47 +1,49 @@
 require 'spec_helper'
 require 'string_calculator'
 
-describe StringCalculator do
-	def string_calculator
-		@string_calculator
-	end
-
-	before(:each) do
-		@string_calculator = StringCalculator.new
-	end
-
-	describe "calc" do
-		it "returns 0 when given an empty string" do
-			string_calculator.calc("").should eql 0
+module StringCalculator
+	describe StringCalculator do
+		def string_calculator
+			@string_calculator
 		end
 
-		it "returns the number from the string when given a single number" do
-			string_calculator.calc("2").should eql 2
+		before(:each) do
+			@string_calculator = StringCalculator.new
 		end
 
-		it "returns the sum of two comma delimited numbers" do
-			string_calculator.calc("2,3").should eql 5
-		end
+		describe "calc" do
+			it "returns 0 when given an empty string" do
+				string_calculator.calc("").should eql 0
+			end
 
-		it "returns the sum of two newline delimited numbers" do 
-			string_calculator.calc("2\n3").should eql 5
-		end
+			it "returns the number from the string when given a single number" do
+				string_calculator.calc("2").should eql 2
+			end
 
-		it "returns the sum of three numbers delimited either way" do
-			string_calculator.calc("2\n3,4").should eql 9
-		end
+			it "returns the sum of two comma delimited numbers" do
+				string_calculator.calc("2,3").should eql 5
+			end
 
-		it "throws an exception if the string contains a negative number" do
-			expect { string_calculator.calc("-2") }.to raise_error(RuntimeError)
-		end
+			it "returns the sum of two newline delimited numbers" do 
+				string_calculator.calc("2\n3").should eql 5
+			end
 
-		it "ignores numbers greater than 1000" do
-			string_calculator.calc("1001").should eql 0
-		end
+			it "returns the sum of three numbers delimited either way" do
+				string_calculator.calc("2\n3,4").should eql 9
+			end
 
-		#A single char delimiter can be defined on the first line (e.g. //# for a ‘#’ as the delimiter)
-		it "interprets delimiters appearing on the first line" do
-			string_calculator.calc("//#\n2#3").should eql 5
+			it "throws an exception if the string contains a negative number" do
+				expect { string_calculator.calc("-2") }.to raise_error(RuntimeError)
+			end
+
+			it "ignores numbers greater than 1000" do
+				string_calculator.calc("1001").should eql 0
+			end
+
+			#A single char delimiter can be defined on the first line (e.g. //# for a ‘#’ as the delimiter)
+			it "interprets delimiters appearing on the first line" do
+				string_calculator.calc("//#\n2#3").should eql 5
+			end
 		end
 	end
 end
